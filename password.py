35a01d22d07d83b6fd043372bb9a70fcb59fc159
Allen Chang <blue69221@gmail.com>
2022-10-19 15:32:03 +0800

    update code use while True

--- password.py
+++ password.py
@@ -1,13 +1,17 @@
 key = 'a123456'
-x = 0
+i = 3
 
-while x < 3:
+while True:
     password = input('請輸入使用密碼：')
     if password == key:
         print('登入成功')
+        break # 離開迴圈
     else:
-        x += 1
-        print(f'密碼錯誤! 還有 {3 - x} 次機會')
-print('帳號鎖定，請洽詢相關人員')
+        i -= 1
+        print(f'密碼錯誤! 還有 {i} 次機會')
+        if i == 0:
+            print('密碼錯誤 3 次，請洽詢相關人員')
+            break
+
 
 
