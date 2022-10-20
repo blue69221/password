ab2aff3c4fe5ee310991ab74b2946d3afe6934f1
Allen Chang <blue69221@gmail.com>
2022-10-20 09:53:37 +0800

    finial code

--- password.py
+++ password.py
@@ -1,17 +1,18 @@
-key = 'a123456'
-i = 3
+password = 'a123456'
+i = 3 # 剩餘機會
 
 while i > 0:
-    password = input('請輸入使用密碼：')
-    if password == key:
+    i -= 1
+    pwd = input('請輸入密碼：')
+    if pwd == password:
         print('登入成功')
         break # 離開迴圈
     else:
-        i -= 1
-        print(f'密碼錯誤! 還有 {i} 次機會')
-        if i == 0:
-            print('密碼錯誤3次，請洽詢相關人員')
-            break
+        print('密碼錯誤')
+        if i > 0:
+            print(f'還有 {i} 次機會')
+        else:
+            print('密碼錯誤過多，請洽詢相關人員')
 
 
 
